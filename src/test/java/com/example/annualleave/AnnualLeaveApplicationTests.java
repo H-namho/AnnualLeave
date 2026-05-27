a16456d33d7b4b3d0a6206a243585d2f0314bf41
H-namho <skagh9696@gmail.com>
2026-05-27 09:21:44 +0900

    main

---
+++ src/test/java/com/example/annualleave/AnnualLeaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.annualleave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnualLeaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
